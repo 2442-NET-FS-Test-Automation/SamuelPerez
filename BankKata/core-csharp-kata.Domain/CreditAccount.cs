aeabbe596a6d56da53b8058ed2197b218e9b0913
Skeiomst <samuelpalfaro@gmail.com>
2026-06-14 22:34:29 -0600

    CreditAccount class added.

---
+++ BankKata/core-csharp-kata.Domain/CreditAccount.cs
@@ -0,0 +1,32 @@
+namespace CoreCsharpKata.Domain;
+
+public class CreditAccount : Account
+{
+    #region Properties
+    public decimal CreditLine {get; set;}
+    #endregion Properties
+
+    #region Constructor
+    public CreditAccount(string client, decimal balance, uint numberAccount, decimal creditLine) : base(client, balance, numberAccount)
+    {
+        CreditLine = creditLine;
+    }
+    #endregion Constructor
+
+    #region Methods
+    public override void Withdraw(decimal amount)
+    {
+        if (amount > 0 && (Balance - amount) >= -CreditLine)
+        {
+            Balance -= amount;
+            Console.WriteLine($"Withdrawal with credit applied. New balance: {Balance}");
+        }
+        else
+        {
+            Console.WriteLine("Exceeds the credit line limit.");
+        }
+    }
+
+    #endregion Methods
+
+}
